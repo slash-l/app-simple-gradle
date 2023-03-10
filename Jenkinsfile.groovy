9f356a61249537263f8de49d0769b33fba2cc347
slash-l <slash_mumu@163.com>
2023-03-10 16:16:05 +0800

    update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,28 +19,8 @@ node {
         rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'clean artifactoryPublish --stacktrace', buildInfo: buildInfo
     }
 
-    stage ("Collect properties") {
-        rtGradle.deployer.addProperty("status", "in-qa").addProperty("compatibility", "1", "2", "3")
-    }
-
     stage ('Publish') {
         server.publishBuildInfo buildInfo
-    }
-
-    stage ("Xray scan") {
-        def xrayConfig = [
-                'buildName': env.JOB_NAME,
-                'buildNumber': env.BUILD_NUMBER,
-                'failBuild': false
-        ]
-        def xrayResults = server.xrayScan xrayConfig
-//          echo xrayResults as String
-        xrayurl = readJSON text:xrayResults.toString()
-//          echo xrayurl as String
-        rtGradle.deployer.addProperty("scan", "true")
-        rtGradle.deployer.addProperty("xrayresult.summary.total_alerts", xrayurl.summary.total_alerts as String)
-
-        rtGradle.deployer.deployArtifacts buildInfo
     }
 
 }
