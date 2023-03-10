57e841e1d90059392ff94e460ddd05ab95078a21
slash-l <slash_mumu@163.com>
2023-03-10 15:13:13 +0800

    update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,56 @@ node {
         rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'clean artifactoryPublish --stacktrace', buildInfo: buildInfo
     }
 
+    stage ("Collect properties") {
+        rtGradle.deployer.addProperty("status", "in-qa").addProperty("compatibility", "1", "2", "3")
+    }
+
     stage ('Publish') {
         server.publishBuildInfo buildInfo
+    }
+
+    stage ("Xray scan") {
+        stage('Xray Scan') {
+            def xrayConfig = [
+                    'buildName': env.JOB_NAME,
+                    'buildNumber': env.BUILD_NUMBER,
+                    'failBuild': true
+            ]
+            def xrayResults = server.xrayScan xrayConfig
+//          echo xrayResults as String
+            xrayurl = readJSON text:xrayResults.toString()
+//          echo xrayurl as String
+            rtGradle.deployer.addProperty("scan", "true")
+            rtGradle.deployer.addProperty("xrayresult.summary.total_alerts", xrayurl.summary.total_alerts as String)
+        }
+    }
+
+    stage ("Promotion") {
+        def promotionConfig = [
+                // Mandatory parameters
+                'targetRepo'         : 'slash-gradle-test-local',
+
+                // Optional parameters
+
+                // The build name and build number to promote. If not specified, the Jenkins job's build name and build number are used
+                'buildName'          : buildInfo.name,
+                'buildNumber'        : buildInfo.number,
+                // Only if this build is associated with a project in Artifactory, set the project key as follows.
+                'project': 'my-project-key',
+                // Comment and Status to be displayed in the Build History tab in Artifactory
+                'comment'            : 'this is the promotion comment',
+                'status'             : 'Released',
+                // Specifies the source repository for build artifacts.
+                'sourceRepo'         : 'slash-gradle-dev-local',
+                // Indicates whether to promote the build dependencies, in addition to the artifacts. False by default
+                'includeDependencies': true,
+                // Indicates whether to copy the files. Move is the default
+                'copy'               : true,
+                // Indicates whether to fail the promotion process in case of failing to move or copy one of the files. False by default.
+                'failFast'           : true
+        ]
+
+        // Promote build
+        server.promote promotionConfig
     }
 }
